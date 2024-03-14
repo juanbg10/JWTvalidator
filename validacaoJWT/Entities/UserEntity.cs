b47ad7d296645d95be7728c4fa44340542e16fc1
juanbg10 <juanbertoluzzigarcia@gmail.com>
2024-03-14 11:24:41 -0300

    feat:creating entities

---
+++ validacaoJWT/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace validacaoJWT.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+    }
+}
